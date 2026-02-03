fffa6ca4b4928af1ab709ada8ec6149e97abf531
HUANG ZHENJIA <E1352450@u.nus.edu>
2026-02-03 20:49:52 +0800

    feat:定义 Gorm Models (User, Task)

---
+++ internal/model/task.go
@@ -0,0 +1,13 @@
+package model
+
+import "gorm.io/gorm"
+
+type task struct {
+	gorm.Model
+	Title    string `gorm:"not null" json:"title"`
+	Content  string `gorm:"longtext" json:"content"`
+	Status   int    `gorm:"default:0" json:"status"`
+	Category string `json:"category"`
+	User     User   `gorm:"ForeignKey:UserId" json:"user,omitempty"`
+	UserId   uint   `gorm:"not null" json:"user_id"`
+}
